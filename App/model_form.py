bc5bb6024d98dfe95542a3a4e295ae94b87c8c89
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2023-02-26 23:54:14 +0530

    feat: create CURD &  model_forms.py file for Create work

---
+++ App/model_form.py
@@ -0,0 +1,7 @@
+from App.models import detail_dataBase
+from django import forms
+
+class Model_forms(forms.ModelForm):
+    class Meta:
+        model = detail_dataBase
+        fields = ['No','Name', 'Address','Image', 'Links', 'Salary' ]
--- App/views.py
+++ App/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from App.models import detail_dataBase
 from App.forms import detail_dataBase_Froms
+from App.model_form import Model_forms
 # Create your views here.
 
 
 def Home (request):
     form = detail_dataBase_Froms()
     datas = detail_dataBase.objects.all()
-    return render(request,"index.html",{"data":datas,'A' :form})
+    model_form = Model_forms()
+    if request.method ==  "POST":
+        Database = Model_forms(request.POST)
+        if Database.is_valid():
+            Database.save()
+            redirect('index')
+    return render(request,"index.html",{"data":datas,'A' :form, 'model_form':model_form})
